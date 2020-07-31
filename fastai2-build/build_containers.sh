ebbe62ee1eb24d81d0f9d042c033b365d590516e
Hamel Husain <hamel.husain@gmail.com>
2020-07-31 11:43:23 -0700

    updated scripts

--- fastai2-build/build_containers.sh
+++ fastai2-build/build_containers.sh
@@ -6,6 +6,11 @@ export IMAGE_NAME_DEV="fastdotai/fastai2-dev"
 export VERSION="0.0.22"
 export DOCKER_BUILDKIT=1
 
+git clone --depth 1 https://github.com/fastai/fastai2.git
+cd fastai2
+mkdir -p docker
+cp -r ../fastai2-build docker/fastai2-build
+
 # build production image
 docker build --cache-from ${IMAGE_NAME}:latest -t ${IMAGE_NAME}:latest --build-arg BUILDKIT_INLINE_CACHE=1 -f docker/fastai2-build/Dockerfile .
 docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${VERSION}
