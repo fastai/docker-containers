0c6cfeb94e10500c0d8741d1507d17c6d3dd8d23
Hamel Husain <hamelsmu@github.com>
2020-08-07 07:25:10 -0700

    Update download_testdata.py

--- fastai2-ci/download_testdata.py
+++ fastai2-ci/download_testdata.py
@@ -7,4 +7,4 @@ from fastai2.torch_core import parallel
 urls = ['ADULT_SAMPLE','BIWI_SAMPLE','CAMVID_TINY','CIFAR','COCO_SAMPLE','COCO_TINY','IMDB','IMDB_SAMPLE',
         'ML_SAMPLE','MNIST','MNIST_SAMPLE','MNIST_TINY','PETS']
 url_list = [URLs.__dict__[k] for k in urls]
-parallel(download_data, url_list, progress=True)
+files = [(print(f'Downloading {u}'), download_data(u)) for u in url_list]
