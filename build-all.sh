970e0120bd98ec86870259db1daed30f151af0e3
Hamel Husain <hamel.husain@gmail.com>
2020-08-29 14:42:40 -0700

    clean up script

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -e
+cd $(dirname "$0") #make sure we are in the root of the repo
+
 for d in ubuntu jekyll miniconda nbdev; do
     cd $d
     echo "building $d"
     ../build.sh
     ../push.sh
 done
-
