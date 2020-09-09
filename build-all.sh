a11ecaa5ae16fc7219bf9d68e10694cbe61d7004
Hamel Husain <hamel.husain@gmail.com>
2020-09-09 07:05:32 -0700

    add codespaces container

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,7 @@
 set -e
 cd $(dirname "$0") #make sure we are in the root of the repo
 
-for d in ubuntu jekyll fastai; do
+for d in ubuntu jekyll fastai codespaces; do
     cd $d
     echo "building $d"
     ../build.sh
