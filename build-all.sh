9ffb84c9cab5b51e39542a45d82bea9314f85a47
Jeremy Howard <j@fast.ai>
2020-09-08 19:04:36 -0700

    slimmer images

--- build-all.sh
+++ build-all.sh
@@ -2,8 +2,7 @@
 set -e
 cd $(dirname "$0") #make sure we are in the root of the repo
 
-for d in ubuntu jekyll miniconda nbdev fastai; do
-#for d in miniconda nbdev fastai; do
+for d in ubuntu jekyll fastai; do
     cd $d
     echo "building $d"
     ../build.sh
