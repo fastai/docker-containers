b445e81006a4ee7e392a30fa291e11e304eb47af
Jeremy Howard <j@fast.ai>
2020-09-09 10:54:35 -0700

    interactive

---
+++ codespaces/tmp_scripts/.bash_aliases
@@ -0,0 +1,6 @@
+alias a=alias
+
+a issue="gh issue create"
+a enhancement="gh issue create -l enhancement -b '' -t"
+a bug="gh issue create -l bug -b '' -t"
+
--- codespaces/tmp_scripts/script.sh
+++ codespaces/tmp_scripts/script.sh
@@ -2,14 +2,15 @@ yes | unminimize
 
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:apt-fast/stable
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-apt-add-repository -y https://cli.github.com/packages
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+#apt-add-repository -y https://cli.github.com/packages
 apt-get update
 apt-get -y install apt-fast apt-utils
 #apt-fast install -y gh
 apt-fast install -y vim-nox ack-grep pigz
 
 pip install -U watchdog[watchmedo]
+rm -f download_testdata.py
 
 echo 'syntax on' > .vimrc
 
