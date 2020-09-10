44a80e877d9d93d2ad0c9ce2ed3907472fa15672
Jeremy Howard <j@fast.ai>
2020-09-10 13:44:59 -0700

    gh

--- codespaces/tmp_scripts/script.sh
+++ codespaces/tmp_scripts/script.sh
@@ -1,13 +1,12 @@
 yes | unminimize
 
-apt-get install -y software-properties-common
+apt-get install -y software-properties-common dirmngr --install-recommends
 add-apt-repository -y ppa:apt-fast/stable
-#apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-#apt-add-repository -y https://cli.github.com/packages
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+apt-add-repository -y https://cli.github.com/packages
 apt-get update
 apt-get -y install apt-fast apt-utils
-#apt-fast install -y gh
-apt-fast install -y vim-nox ack-grep pigz
+apt-fast install -y vim-nox ack-grep pigz gh
 
 pip install -U watchdog[watchmedo] bash_kernel
 python -m bash_kernel.install
