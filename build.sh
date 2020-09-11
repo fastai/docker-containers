a4a3c406cb930f1cc026cdbefcddd248a9271cc0
Hamel Husain <hamelsmu@github.com>
2020-09-10 21:38:49 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+echo "::group::Building ${PWD##*/}"  
 echo ${PWD##*/}:latest
 echo $(<image)
 docker build . -t fastai/${PWD##*/}:latest -t fastai/${PWD##*/}:$(date +%F) --build-arg IMAGE=$(<image) -f ../Dockerfile
-
+echo "::endgroup::"
