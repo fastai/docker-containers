2dc0bc77d9bcf44783226dc3391d146361005618
Hamel Husain <hamelsmu@github.com>
2020-09-10 23:10:52 -0700

    Update script.sh

--- jekyll/tmp_scripts/script.sh
+++ jekyll/tmp_scripts/script.sh
@@ -4,8 +4,9 @@ apt install -y ruby-full
 gem install jekyll bundler
 bundle config set system 'true'
 bundle install
+wget https://github.com/jgm/pandoc/releases/download/2.9.1.1/pandoc-2.9.1.1-1-amd64.deb
+dpkg -i pandoc-2.9.1.1-1-amd64.deb
 rm -rf Gemfile*
 rm -rf /var/lib/gems/*/cache
 rm -rf /var/lib/gems/*/doc
 rm -rf /usr/share/ri
-
