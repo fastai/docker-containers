550b81b5512d0f544d62721582189bdeffdd0ae6
Hamel Husain <hamelsmu@github.com>
2020-09-10 21:25:02 -0700

    Update script.sh

--- ubuntu/tmp_scripts/script.sh
+++ ubuntu/tmp_scripts/script.sh
@@ -1,7 +1,7 @@
 mv 99local /etc/apt/apt.conf.d/
 apt-get -y upgrade
 apt-get -y install apt-utils
-apt-get install -y --no-install-recommends wget bzip2 ca-certificates git rsync build-essential curl grep sed dpkg sudo perl wget libglib2.0-dev zlib1g-dev lsb-release tmux less htop ctags openssh-client vim-tiny python-is-python3 python3-pip python3-dev
+apt-get install -y --no-install-recommends wget bzip2 ca-certificates git rsync build-essential curl grep sed dpkg sudo perl wget libglib2.0-dev zlib1g-dev lsb-release tmux less htop ctags openssh-client vim-tiny python-is-python3 python3-pip python3-dev dos2unix
 
 python -m pip install -U pip 
 echo 'set nocompatible' > .vimrc
