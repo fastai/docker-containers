578063a6f307629dbb0e0c684dcbb0ccfc1cc004
Hamel Husain <hamelsmu@github.com>
2020-11-16 09:03:39 -0800

    Update script.sh

--- fastai/tmp_scripts/script.sh
+++ fastai/tmp_scripts/script.sh
@@ -1,8 +1,8 @@
 apt update
-apt install software-properties-common dirmngr gpg-agent -y
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
-apt-add-repository https://cli.github.com/packages
-apt install -y jq gh
+# apt install software-properties-common dirmngr gpg-agent -y
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C99B11DEB97541F0
+# apt-add-repository https://cli.github.com/packages
+# apt install -y jq gh
 PT_VER=$(curl -s https://pypi.org/pypi/torch/json | jq -r .info.version)
 TV_VER=$(curl -s https://pypi.org/pypi/torchvision/json | jq -r .info.version)
 pip install torch==$PT_VER+cpu torchvision==$TV_VER+cpu -f https://download.pytorch.org/whl/torch_stable.html
