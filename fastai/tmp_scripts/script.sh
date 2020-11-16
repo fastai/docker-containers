ec9a4cb2c55d25af591974c4ef33ed1092c2d5ef
Hamel Husain <hamelsmu@github.com>
2020-11-16 14:17:47 -0800

    Update script.sh

--- fastai/tmp_scripts/script.sh
+++ fastai/tmp_scripts/script.sh
@@ -6,6 +6,6 @@ apt update
 PT_VER=$(curl -s https://pypi.org/pypi/torch/json | jq -r .info.version)
 TV_VER=$(curl -s https://pypi.org/pypi/torchvision/json | jq -r .info.version)
 pip install torch==$PT_VER+cpu torchvision==$TV_VER+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip install ipywidgets fastai
+pip install ipywidgets fastai neptune wandb pydicom tensorboard captum
 # jupyter nbextension enable --py widgetsnbextension
 
